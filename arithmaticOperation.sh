be1b297782afec24d1565cfd965ae91558bc5238
Devki1 <gdev3195@gmail.com>
2020-02-26 10:23:47 +0530

    ADDED:Take a three input from a user a b and c

---
+++ arithmaticOperation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+#Enter a three number by a user
+read -p "enter the number a:" a
+read -p "enter the number b:" b
+read -p "enter the number c:" c
+echo "number1:"=$a
+echo "number2:"=$b
+echo "number3:"=$c
