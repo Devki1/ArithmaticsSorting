8fe96d683ad8e923dd592f38b472e80ef8bfbdb6
Devki1 <gdev3195@gmail.com>
2020-02-26 11:14:02 +0530

    ADDED:Perform arithmatic operation

--- arithmaticOperation.sh
+++ arithmaticOperation.sh
@@ -4,7 +4,8 @@ read -p "enter the number a:" a
 read -p "enter the number b:" b
 read -p "enter the number c:" c
 number2=`echo "scale=2 ; $a * $b +$c" | bc -l`
-number3=`echo "scale=2 ; $c + $a /$b " | bc -l`
-echo "number3:"=$number3
+number3=`echo "scale=2 ; $c + $a / $b " | bc -l`
+number4=`echo "scale=2 ; $a % $b + $c " | bc -l`
+echo "number4:"=$number4
 
 
