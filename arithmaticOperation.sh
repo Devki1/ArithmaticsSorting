7fb77406098089d7d7122ff63b4ab68cf2c50f19
Devki1 <gdev3195@gmail.com>
2020-02-26 11:29:17 +0530

    ADDED:Store all the result in a dictionary

--- arithmaticOperation.sh
+++ arithmaticOperation.sh
@@ -3,9 +3,15 @@
 read -p "enter the number a:" a
 read -p "enter the number b:" b
 read -p "enter the number c:" c
+declare -A arithmaticDictionary 
+#Compute the number and store the dictionary
+number1=`echo "scale=2 ; $a + $b * $c" | bc -l`
+arithmaticDictionary[1]=$number1
 number2=`echo "scale=2 ; $a * $b +$c" | bc -l`
+arithmaticDictionary[2]=$number2
 number3=`echo "scale=2 ; $c + $a / $b " | bc -l`
+arithmaticDictionary[3]=$number3
 number4=`echo "scale=2 ; $a % $b + $c " | bc -l`
-echo "number4:"=$number4
+arithmaticDictionary[4]=$number4
 
 
